3cedc255fa40ce10b3e4a434d92f627a82b0f5f4
JoseAlva115 <jdalvarad@gmail.com>
2024-11-10 11:47:54 -0600

    feat: dashboards
    implementación de dashboards y vistas relacionadas a estas

---
+++ PokemonApp/Controllers/EntrenadorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using PokemonApp.Models;
+
+namespace PokemonApp.Controllers
+{
+    public class EntrenadorController : Controller
+    {
+
+        private PokemondbContext db = new PokemondbContext();
+
+
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId<int>();
+            var pokemons = db.Pokemons.Where(p => p.UsuarioId == userId).ToList(); 
+            var totalPokemons = pokemons.Count();
+            var pokemonsByType = pokemons.GroupBy(p => p.Tipo)
+                                         .Select(g => new { Tipo = g.Key, Count = g.Count() })
+                                         .ToList();
+
+            ViewBag.TotalPokemons = totalPokemons;
+            ViewBag.PokemonsByType = pokemonsByType;
+
+            return View(pokemons);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PokemonApp/Models/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PokemonApp.Models
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public int RolId { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Nombre { get; set; }
+        public Role Rol { get; set; }
+    }
